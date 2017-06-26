d1065e9b6ecd6fc646a922090709440fec681567
Paul Curtis <paul@unomee.com>
2017-06-26 16:16:28 +0100

    working table

--- routes/users.js
+++ routes/users.js
@@ -11,5 +11,16 @@ router.get('/userlist', function(req, res) {
         res.json(docs);
     });
 });
-
+/*
+ * POST to adduser.
+ */
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
 module.exports = router;
