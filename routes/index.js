1fad885f940310135c6930fa7a288d0ba7a4ebaa
Paul Curtis <paul@unomee.com>
2017-06-27 15:21:16 +0100

    some lanky feet

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome to Convert Stuff' });
+});
+
+router.get('/order', function(req, res, next) {
+  res.render('order', { title: 'New Order' });
 });
 
 module.exports = router;
